a22a813d1d41c34b5ef11b6e63a9126cd040616d
Brian Holt <bharrisonholt@gmail.com>
2020-05-05 13:05:10 -0700

    Updates

---
+++ precache-manifest.795e375d98ff0b41c05172522d966ea4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bcdc8370b572fc68e2f96be996db9a",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "ee21b2a4aaa0b13e5d7b",
+    "url": "/BudgetApp/static/css/2.49354865.chunk.css"
+  },
+  {
+    "revision": "2f0d4a91cde7db2e967d",
+    "url": "/BudgetApp/static/css/main.d1d3bcaf.chunk.css"
+  },
+  {
+    "revision": "ee21b2a4aaa0b13e5d7b",
+    "url": "/BudgetApp/static/js/2.de01533a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/BudgetApp/static/js/2.de01533a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0d4a91cde7db2e967d",
+    "url": "/BudgetApp/static/js/main.9ef2f334.chunk.js"
+  },
+  {
+    "revision": "a7f83fbf5b6a3b51bfbf",
+    "url": "/BudgetApp/static/js/runtime-main.373dc8d7.js"
+  },
+  {
+    "revision": "a1e509fbd512fd1f4a94918d1aaeed44",
+    "url": "/BudgetApp/static/media/background2.a1e509fb.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/BudgetApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/BudgetApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/BudgetApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/BudgetApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/BudgetApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
